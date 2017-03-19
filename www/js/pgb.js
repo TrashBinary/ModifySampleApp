d9464c924564ae99f12a22225340c6da0c9307cf
TosiekTosiek <l763037@mvrht.com>
2017-03-19 15:33:56 +0100

    poprawska w javaScript

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -10,18 +10,18 @@ function deviceInfo() {
 
 	info =  'Hi, I am your smartphone :-)' + '\n' +
 			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
+			'Device Name    : '     + device.name     + '\n' +
+			'Device Cordova : '  + device.cordova + '\n' +
+			'Device Platform: ' + device.platform + '\n' +
+			'Device UUID    : '     + device.uuid     + '\n' +
+			'Device Model   : '    + device.model     + '\n' +
 			'Device Version : '  + device.version  + '\n';
 
 	navigator.notification.alert(info);
-	
+
 	function mojeDaneOsobowe() {
-		dane = "Imienia i nazwiska nie podaje.";
+		dane = 'Imienia i nazwiska nie podaje.';
 	}
-	
+
 	navigator.notification.alert(dane);
 }
